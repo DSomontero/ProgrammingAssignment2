60b60fcbaa105def7653bd158ec603a91c175f31
DSomontero <omontero@gmail.com>
2015-11-21 19:06:25 -0400

    add program for coursera programming practice 2: cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -5,11 +5,34 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
-}
+   # This function first store an inverted matrix in the cache for later use.
+   # It initializes the matrix to NULL. Then creates the matrix in the working environment
+   #Inverse it and stored in cache		
 
+        cached_Matrix <- NULL
+        emptymatrix <- function(y) {
+                x <<- y
+                cached_Matrix <<- NULL
+        }
+        matrixvalue <- function() x
+        BaseMatrix <- function(inverse) cached_Matrix <<- inverse
+        Inverse_Matrix <- function() cached_Matrix
+        list(emptymatrix = emptymatrix, matrixvalue = matrixvalue, BaseMatrix = BaseMatrix, Inverse_Matrix = Inverse_Matrix)
+}
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# Returns a the inverse matrix of 'x' using the cached matrix of makeCacheMatrix(). If it has been calculated use the data from cache and skip calculation. 
+        
+        InvMatrix = x$getinv()
+        if (!is.null(InvMatrix)){
+                 
+                message("getting cached data")
+                return(InvMatrix)
+        }
+        mat.data = x$get()
+        InvMatrix = solve(mat.data, ...)
+        x$setinv(InvMatrix)
+        return(InvMatrix)
 }
